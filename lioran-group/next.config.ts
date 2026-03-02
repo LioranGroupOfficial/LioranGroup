5884809d1f8a0df53cb4621ca13ddd686997581b
UltronTheAI <coderswaraj@gmail.com>
2026-03-02 12:21:21 +0530

    Fixed SEO

--- lioran-group/next.config.ts
+++ lioran-group/next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.lioran.group',
+          },
+        ],
+        destination: 'https://lioran.group/:path*',
+        permanent: true,
+      },
+    ];
+  }
 };
 
 export default nextConfig;
